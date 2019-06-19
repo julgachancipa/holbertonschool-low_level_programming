fff1e078a3f66e92badedb58e749bb68ef98103d
julgachancipa <julianaga01@gmail.com>
2019-06-19 17:41:30 -0500

    Print with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - Show in display a message use put
+*Return: return 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
