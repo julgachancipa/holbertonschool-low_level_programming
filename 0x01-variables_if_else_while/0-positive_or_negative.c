05d57935bec222f1c636de99cc12e12ebfc2c1b9
julgachancipa <julianaga01@gmail.com>
2019-06-20 14:33:28 -0500

    update file numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive  \n", n);
+		printf("%d is %s\n", n, "positive");
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative \n", n);
+		printf("%d is %s\n", n, "negative");
 	}
 	else
 	{
-		printf("%d is zero \n", n);
+		printf("%d is %s\n", n, "zero");
 	}
 	return (0);
 }
