b22b8b38695a0987c30d55ab506c4fb549847ee1
julgachancipa <julianaga01@gmail.com>
2019-06-20 12:41:21 -0500

    number class

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
 * main - show if a number is positive, negative, or 0
 * Return: 0
@@ -12,15 +13,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n == 0)
 {
-printf("%d is zero", n);
+printf("%d is zero \n", n);
 }
 else if (n < 0)
 {
-printf("%d is negative", n);
+printf("%d is negative \n", n);
 }
 else if (n > 0)
 {
-printf("%d is positive", n);
+printf("%d is positive \n", n);
 }
 return (0);
 }
