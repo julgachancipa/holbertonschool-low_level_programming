cda675b88c92f712b882b46817afed86dd0b21fe
julgachancipa <julianaga01@gmail.com>
2019-06-20 22:32:13 -0500

    double num

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - prints 0 to 10 numbers
+ *Return: 0
+*/
+int main(void)
+{
+	int n;
+	int i;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			putchar(n);
+			putchar(i);
+			if (!(n == '9' && i == '9'))
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
