954bc64756c546b381692356217f61dc00c9755a
julgachancipa <julianaga01@gmail.com>
2019-06-22 12:25:36 -0500

    comb4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = '0'; b <= '8'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
 			for (c = '0'; c <= '9'; c++)
 			{
-				for (d = '1'; d <= '9'; d++)
+				for (d = '0'; d <= '9'; d++)
 				{
 					num1 = (((int) a - '0') * 10) + ((int) b - '0');
 					num2 = (((int) c - '0') * 10) + ((int) d - '0');
@@ -29,7 +29,7 @@ int main(void)
 						putchar(' ');
 						putchar(c);
 						putchar(d);
-					if (!(a == '9' && b == '8' && c == '9' && d == '9'))
+					if (!(num1 == 98 && num2 == 99))
 					{
 						putchar(',');
 						putchar(' ');
