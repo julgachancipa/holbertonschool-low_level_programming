77fedef70f5646ec212f9963dc7fa80a6bf472cf
julgachancipa <julianaga01@gmail.com>
2019-06-22 12:01:06 -0500

    num0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - print num 0-9
+ *Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
