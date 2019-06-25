c13eb7b52f87553182298425474168a2595bf7e3
julgachancipa <julianaga01@gmail.com>
2019-06-24 23:57:52 -0500

    mandatory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *add - addition
+ *@a: parameter
+ *@b: parameter
+ *Return: c
+*/
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+ *print_to_98 - from n to 98
+ *@n: parameter
+ *Return: ntg
+*/
+void print_to_98(int n)
+{
+	int a, b, c;
+
+	while (n < 98 || n > 98)
+	{
+		c = n % 10;
+		b = ((n % 100) - c) / 10;
+		a = (n - c - b) / 100;
+		if (n < 0)
+		{
+			a = a * -1;
+			b = b * -1;
+			c = c * -1;
+			_putchar('-');
+			n++;
+		}
+		else if (n > 98)
+		{
+			n--;
+		}
+		else if (n >= 0 && n < 98)
+		{
+			n++;
+		}
+		if (n > 98 || n < -98)
+		{
+			_putchar(a + '0');
+		}
+		if (n > 8 || n < -8)
+		{
+			_putchar(b + '0');
+		}
+		_putchar(c + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
