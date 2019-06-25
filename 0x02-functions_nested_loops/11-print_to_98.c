542c79504688dd94d61b1ffdcc2bf9bca0217a78
julgachancipa <julianaga01@gmail.com>
2019-06-25 15:23:20 +0000

    9

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,38 +8,29 @@ void print_to_98(int n)
 {
 	int a, b, c;
 
-	while (n < 98 || n > 98)
+	while (n != 98)
 	{
 		c = n % 10;
 		b = ((n % 100) - c) / 10;
 		a = (n - c - b) / 100;
 		if (n < 0)
 		{
-			a = a * -1;
-			b = b * -1;
 			c = c * -1;
+			b = b * -1;
+			a = b * -1;
 			_putchar('-');
-			n++;
 		}
-		else if (n > 98)
-		{
-			n--;
-		}
-		else if (n >= 0 && n < 98)
-		{
-			n++;
-		}
-		if (n > 98 || n < -98)
-		{
+		if ((n < -99 || n > 99) && n != 0)
 			_putchar(a + '0');
-		}
-		if (n > 8 || n < -8)
-		{
+		if ((n < -9 || n > 9) && n != 0)
 			_putchar(b + '0');
-		}
 		_putchar(c + '0');
 		_putchar(',');
 		_putchar(' ');
+		if (n < 98)
+			n++;
+		else if (n > 98)
+			n--;
 	}
 	_putchar('9');
 	_putchar('8');
