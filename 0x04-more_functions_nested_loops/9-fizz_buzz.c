ca2cb9c3f816861c94a020b9db67f2ad5e91276d
julgachancipa <julianaga01@gmail.com>
2019-06-27 18:14:45 +0000

    9 again

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,18 +10,16 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{
-			if (i % 3 == 0)
-				printf("Fizz");
-			if (i % 5 == 0)
-				printf("Buzz");
-		}
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
 		else
-		{
 			printf("%d", i);
-		}
-		printf(" ");
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
