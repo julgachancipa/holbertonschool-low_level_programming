fe103a5d2665558027a5fd2a32ac917d55b95492
julgachancipa <julianaga01@gmail.com>
2019-06-27 21:12:21 +0000

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,17 @@
  */
 void print_number(int n)
 {
-	char a, b, c, d;
+	int a, b, c, d;
 
 	if (n < 0)
 	{
 		n = n * -1;
-		_putchar('-');
+		_putchar(45);
 	}
-	d = n % 10;
-	c = ((n % 100) - d) / 10;
-	b = ((n % 1000) - c - d) / 100;
-	a = (n - b - c - d) / 1000;
+	d = (n % 10);
+	c = (((n % 100) - d) / 10);
+	b = (((n % 1000) - c - d) / 100);
+	a = ((n - b - c - d) / 1000);
 	if (n > 999 || n < -999)
 		_putchar(a + '0');
 	if (n > 99 || n < -99)
