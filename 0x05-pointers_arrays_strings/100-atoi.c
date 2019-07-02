499e0cfc61c1c6031d1bd5a0d5de10cd66fdd3c5
julgachancipa <julianaga01@gmail.com>
2019-07-02 17:30:09 +0000

    10 atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@ int _atoi(char *s)
 {
 	int i;
 	int res = 0;
-	int sig = 1;
+	int sig = -1;
 	int brk = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
@@ -18,7 +18,7 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			res = res * 10;
-			res += (s[i] - '0');
+			res -= (s[i] - '0');
 			brk = 1;
 		}
 		else if (brk == 1)
