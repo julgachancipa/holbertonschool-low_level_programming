5b445355861db0cb4e2955693763d0b501632f28
julgachancipa <julianaga01@gmail.com>
2019-07-02 14:35:28 +0000

    6 again

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,8 +7,15 @@
 void puts2(char *str)
 {
 	int i;
+	int sz = 0;
 
-	for (i = 0; i < 1000; i += 2)
+	for (i = 0; i < 1000; i++)
+	{
+		if (str[i] == '\0')
+			break;
+		sz++;
+	}
+	for (i = 0; i < sz; i += 2)
 	{
 		if (str[i] == '\0')
 			break;
