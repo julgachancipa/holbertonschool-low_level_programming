87a325dd8905e480cf688b99c3ff69a8e6275ce2
julgachancipa <julianaga01@gmail.com>
2019-07-02 15:02:42 +0000

    8 again

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 #include <stdio.h>
 /**
  * print_array - prints until the n element of a string
@@ -8,8 +9,15 @@
 void print_array(int *a, int n)
 {
 	int i;
+	int sz = 0;
 
 	for (i = 0; i < 1000; i++)
+	{
+		if (s[i] == '\0')
+			break;
+		sz++;
+	}
+	for (i = 0; i < sz; i++)
 	{
 		if (i < n)
 			printf("%d", a[i]);
