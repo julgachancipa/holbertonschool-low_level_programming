aa1ac830363e94764ec3322156ef632ad00918bf
julgachancipa <julianaga01@gmail.com>
2019-07-04 21:36:07 +0000

    until 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,13 +7,18 @@
 char *rot13(char *s)
 {
 	int i, j;
-	char l[] = "abcdefghijklmnopqrstuvwxyzabcdefghijklm";
+	int l[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+		   'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+		   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; l[j] != '\0'; j++)
 		{
-			if (s[i] == l[j] || s[i] == (l[j] - 32))
+			if (s[i] == l[j])
 			{
 				s[i] = l[j + 13];
 				break;
