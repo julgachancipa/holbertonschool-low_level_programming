f442ead65341acdcf06750085a9b362ebd892dc5
julgachancipa <julianaga01@gmail.com>
2019-07-04 20:53:41 +0000

    until 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * leet - encodes into 1337.
+ * @s: pointer
+ * Return: encode string.
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char l[] = "O__EA__T";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; l[j] != '\0'; j++)
+		{
+			if (s[i] == l[j] || s[i] == (l[j] + 32))
+				s[i] = j + '0';
+		}
+	}
+	return (s);
+}
