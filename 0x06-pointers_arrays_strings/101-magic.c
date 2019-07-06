e56390d01930a2da0c810ccad4ad171cc2158039
julgachancipa <julianaga01@gmail.com>
2019-07-06 12:06:20 -0500

    101

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-
+/**
+ * main - change value from memory
+ * Return: 0
+*/
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int n;
+	int a[5];
+	int *p;
 
-  a[2] = 1024;
-  p = &n;
-  *(p+5) = 98;
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
