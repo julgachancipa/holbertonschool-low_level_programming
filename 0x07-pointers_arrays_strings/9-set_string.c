b00b62441841d10eefa63eda13ac3841ce28aebf
julgachancipa <julianaga01@gmail.com>
2019-07-08 22:34:37 -0500

    8

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - sets the value of a pointer to a char..
+ * @s: source
+ * @to: destine
+ * Return: ntg.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
