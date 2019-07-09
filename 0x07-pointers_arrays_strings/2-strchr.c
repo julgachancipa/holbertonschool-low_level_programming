08de0f83bd0423cf118622ae699b6cb15aafafd4
julgachancipa <julianaga01@gmail.com>
2019-07-08 20:57:16 -0500

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,17 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i, b = 0;
 
 	for (i = 0; s[i]; i++)
 	{
 		if (s[i] == c)
+		{
 			return (s + i);
+			b = 1;
+		}
 	}
+	if (b == 1)
+		return ('\0');
 	return ('\0');
 }
