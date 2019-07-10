7033fcf3e14541ec757e1ea83dc7899833783391
julgachancipa <julianaga01@gmail.com>
2019-07-10 17:08:29 +0000

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,10 @@ int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (*s == '\0')
-		return (len);
-	else
+	if (*s != '\0')
 	{
 		len++;
 		return (len + _strlen_recursion(s + 1));
 	}
+	return (len);
 }
