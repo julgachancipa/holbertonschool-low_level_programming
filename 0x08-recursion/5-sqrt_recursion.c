9f521e951559d067e7c79d7027131d106059676b
julgachancipa <julianaga01@gmail.com>
2019-07-10 19:52:55 +0000

    5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,25 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - returns the natural square root of a number.
+ * comp - evalue result
  * @n: value
  * @aux: auxiliar
- * Return: result
+ * Return: aux
  */
 int comp(int aux, int n)
 {
 	if (aux * aux == n)
-        {
+	{
 		return (aux);
 	}
 	else if (aux * aux > n)
 		return (-1);
 	return (comp(aux + 1, n));
 }
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: value
+ * Return: result
+ */
 int _sqrt_recursion(int n)
 {
 	return (comp(1, n));
