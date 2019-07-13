902d0d489e172515d5c3ccd983ec213acf210b5a
julgachancipa <julianaga01@gmail.com>
2019-07-13 16:27:25 -0500

    wimn?2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,7 @@
  * main - prints its name, followed by a new line.
  * @argc: number of arguments
  * @argv: array of pointers to the strings (arguments)
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
