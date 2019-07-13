b3b4078a165007ce6cee94cde8e160451badd553
julgachancipa <julianaga01@gmail.com>
2019-07-13 16:51:22 -0500

    arguments1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints num of arg., followed by a new line.
+ * @argc: number of arguments
+ * @argv: array of pointers to the strings (arguments)
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
