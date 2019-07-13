5052dbf5c9176c3b8c3e8ccf985a163c2ae0d5cf
julgachancipa <julianaga01@gmail.com>
2019-07-13 18:51:26 -0500

    add1

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add numbers, followed by a new line.
+ * @argc: number of arguments
+ * @argv: array of pointers to the strings (arguments)
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i, b = 1;
+
+	if (argc == 1)
+		printf("%d\n", sum);
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0 && *argv[i] != '0')
+				b = 0;
+			sum += atoi(argv[i]);
+		}
+		if (b == 1)
+			printf("%d\n", sum);
+		else
+			printf("Error\n");
+	}
+	return (0);
+}
