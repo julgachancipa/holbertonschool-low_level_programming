7cbb48c459afa98ddd37e4b81688776fa5529992
julgachancipa <julianaga01@gmail.com>
2019-07-13 16:25:48 -0500

    wimn?1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: number of arguments
+ * @argv: array of pointers to the strings (arguments)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
