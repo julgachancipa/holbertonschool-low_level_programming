f7918fd77bd9ce0dbd6072bc10590298d68324d6
julgachancipa <julianaga01@gmail.com>
2019-07-16 18:35:23 +0000

    duparray1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: str len
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s != '\0')
+	{
+		len++;
+		return (len + _strlen_recursion(s + 1));
+	}
+	return (len);
+}
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: str size
+ * Return: string
+ */
+char *_strdup(char *str)
+{
+	unsigned int i;
+	char *s;
+
+	s = malloc(_strlen_recursion(str));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; str[i]; i++)
+		{
+			s[i] = str[i];
+		}
+		s[i] = '\0';
+		return (s);
+	}
+}
