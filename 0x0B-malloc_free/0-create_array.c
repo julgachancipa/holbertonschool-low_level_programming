9f5401c65539d230b0ebd8c4d06aacc75c920968
julgachancipa <julianaga01@gmail.com>
2019-07-16 17:24:43 +0000

    initarray2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *s;
 
-	s = malloc(size * sizeof(char));
+	s = malloc((size * sizeof(char)) + 1);
 	for (; i < size; i++)
 	{
 		s[i] = c;
