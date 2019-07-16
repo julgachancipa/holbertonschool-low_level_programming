f41bd274104a962bf8cb80b36419caf3c37926ab
julgachancipa <julianaga01@gmail.com>
2019-07-16 22:17:47 +0000

    alloc3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,6 +17,8 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	a = malloc(sizeof(int *) * height);
+	if (a == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		a[i] = malloc(sizeof(int) * width);
