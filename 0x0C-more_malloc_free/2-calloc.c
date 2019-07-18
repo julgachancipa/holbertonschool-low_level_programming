45ac42172e818c05e68d002c7058627e659605d3
julgachancipa <julianaga01@gmail.com>
2019-07-18 19:42:59 +0000

    calloc1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,4 +11,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *p;
+	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size + 1);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
+	return (p);
+}
