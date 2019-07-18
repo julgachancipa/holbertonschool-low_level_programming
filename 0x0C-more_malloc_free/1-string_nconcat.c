2506c704708d8d02e909da30c1e0fd72b9a94221
julgachancipa <julianaga01@gmail.com>
2019-07-18 18:57:57 +0000

    nconcat5

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n > _strlen(s2))
+	if (n >= _strlen(s2))
 		n = _strlen(s2);
 	len = sz1 + n + 1;
 	s = malloc(len * sizeof(char));
