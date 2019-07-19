020dda4aa1b5aa76d35d87c8a2bc4f06c4d18979
julgachancipa <julianaga01@gmail.com>
2019-07-19 00:52:23 +0000

    100.3

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,19 +14,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *np;
 	unsigned int i;
 
-	if (new_size == old_size)
-	{
-		return (ptr);
-	}
 	if (ptr == NULL)
 	{
 		np = malloc(new_size);
-		return(np);
+		return (np);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
 	}
 	np = malloc(new_size);
 	if (np == NULL)
