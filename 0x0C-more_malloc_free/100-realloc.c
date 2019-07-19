1f8755db3e07a11bdfcf8bf31823de55a145f041
julgachancipa <julianaga01@gmail.com>
2019-07-19 00:53:59 +0000

    100.4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -33,5 +33,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	for (i = 0; i < old_size; i++)
 		np[i] = ((char *)ptr)[i];
+	free(ptr);
 	return (np);
 }
