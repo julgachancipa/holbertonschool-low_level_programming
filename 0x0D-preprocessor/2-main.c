51ae760be7095f2f30f08e7cee0b8b5c8024141e
julgachancipa <julianaga01@gmail.com>
2019-07-22 17:20:01 +0000

    2.2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#define FILE_NAME __FILE__
 /**
  * main - prints the name of the file it was compiled from
  * Return: 0
  */
 int main(void)
 {
-	printf("%s\n", __FILE__);
+	printf("%s\n", FILE_NAME);
 }
