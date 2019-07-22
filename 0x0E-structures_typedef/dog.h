b4008d3c683568e1579a8459b11b1fdf7e4ebb94
julgachancipa <julianaga01@gmail.com>
2019-07-22 20:21:41 +0000

    0.1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Define a dog
+ * @name: Name of the dog
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Defines a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
