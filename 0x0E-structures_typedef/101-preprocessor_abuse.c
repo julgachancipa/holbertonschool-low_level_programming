f5f49aa6ec5287340845b17b72a646d5cbad1ffa
julgachancipa <julianaga01@gmail.com>
2019-07-23 02:19:58 +0000

    101.2

--- 0x0E-structures_typedef/101-preprocessor_abuse.c
+++ 0x0E-structures_typedef/101-preprocessor_abuse.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-#define STR "Hello, Holberton\n"
-/**
- * main - print srt
- *
- * Return: 0
- */
-int main(void)
-{
-	printf(STR);
-	return (0);
-}
+main(){puts("Hello, Holberton");}
