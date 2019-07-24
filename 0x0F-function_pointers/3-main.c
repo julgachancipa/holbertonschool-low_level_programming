b06f619a42ad58d077752bfc57d2969532a66aa7
julgachancipa <julianaga01@gmail.com>
2019-07-24 23:28:52 +0000

    4.5

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,13 +9,16 @@
  */
 int main(int argc, char **argv)
 {
+	char aux = argv[2][0];
+	int a = 0;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
-	     && argv[2][0] != '/' && argv[2][0] != '%')
+	if (aux != '+' && aux != '-' && aux != '*'
+	     && aux != '/' && aux != '%')
 	{
 		printf("Error\n");
 		exit(99);
@@ -29,6 +32,7 @@ int main(int argc, char **argv)
 	int num1 = atoi(argv[1]);
 	int num2 = atoi(argv[3]);
 
-	printf("%d\n", (get_op_func(argv[2]))(num1, num2));
+	a = (get_op_func(argv[2]))(num1, num2);
+	printf("%d\n", a);
 	return (0);
 }
