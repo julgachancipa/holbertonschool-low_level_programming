d957b3cf55da29decc51c174ba31162cc4b5c4fc
julgachancipa <julianaga01@gmail.com>
2019-07-27 17:15:01 +0000

    3th task.1

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,20 +2,94 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
+ * character - pc
+ * @lst: arg
+ * Return: ntg
+ */
+void character(va_list lst)
+{
+	char aux = va_arg(lst, int);
+
+	printf("%c", aux);
+}
+/**
+ * integer - pi
+ * @lst: arg
+ * Return: ntg
+ */
+void integer(va_list lst)
+{
+	int aux = va_arg(lst, int);
+
+	printf("%d", aux);
+}
+/**
+ * floater - pf
+ * @lst: arg
+ * Return: ntg
+ */
+void floater(va_list lst)
+{
+	double aux = va_arg(lst, double);
+
+	printf("%f", aux);
+}
+/**
+ * str - ps
+ * @lst: arg
+ * Return: ntg
+ */
+void str(va_list lst)
+{
+	char *aux = va_arg(lst, char *);
+
+	if (aux != NULL)
+		printf("%s", aux);
+	else
+		printf("(nil)");
+}
+/**
  * print_all - prints anything.
- * @format: list of types of arguments passed to the function
+ * @format:lst of types of arguments passed to the function
  * Return: ntg
  */
 void print_all(const char * const format, ...)
 {
+	int i, j, k;
 	va_list lst;
-	unsigned int i;
+	var_t var[] = {
+		{"c", character},
+		{"i", integer},
+		{"f", floater},
+		{"s", str},
+		{NULL, NULL}
+	};
 
 	va_start(lst, format);
-	while (*ptr != '\0')
+	i = 0;
+	k = 0;
+	while (format[i] != '\0')
 	{
-		printf("%c", *ptr);
-		ptr++;
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == var[j].tp[0])
+			{
+				switch (k)
+				{
+					case 0:
+						break;
+					default:
+						printf(", ");
+				}
+				var[j].f(lst);
+				k++;
+				break;
+
+			}
+			j++;
+		}
+		i++;
 	}
 	printf("\n");
 	va_end(lst);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,18 @@
 #ifndef VA_FUN_H
 #define VA_FUN_H
+#include <stdarg.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+typedef struct vars
+{
+	char *tp;
+	void (*f)();
+}var_t;
+void character(va_list lst);
+void integer(va_list lst);
+void floater(va_list lst);
+void str(va_list lst);
 #endif
