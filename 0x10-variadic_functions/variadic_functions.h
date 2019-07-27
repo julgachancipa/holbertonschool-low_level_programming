2bc667c3b924ce4353876ce74e8493343743819a
julgachancipa <julianaga01@gmail.com>
2019-07-27 17:24:03 +0000

    fixing betty in header files

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,11 +6,18 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+/**
+ * struct vars - character and function
+ * @tp: char
+ * @f: function
+ *
+ * Description: match the char with the function
+ */
 typedef struct vars
 {
 	char *tp;
 	void (*f)();
-}var_t;
+} var_t;
 void character(va_list lst);
 void integer(va_list lst);
 void floater(va_list lst);
