eb987af27b73df93d6930f857390656b344ae34e
julgachancipa <julianaga01@gmail.com>
2019-08-01 21:08:35 +0000

    #Feauture: func. that print a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to header
+ * Return: # of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	do {
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		nodes++;
+		h = h->next;
+	} while (h != NULL);
+	return (nodes);
+}
