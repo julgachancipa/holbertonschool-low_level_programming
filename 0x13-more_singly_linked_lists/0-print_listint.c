83f7693538d3506c1973534e16124ccb86d86654
julgachancipa <julianaga01@gmail.com>
2019-08-05 16:06:09 +0000

    #Feature: print a list that only have an int element

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: head pointer.
+ * Return: # of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
