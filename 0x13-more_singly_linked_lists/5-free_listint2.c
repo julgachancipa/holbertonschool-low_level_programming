c9cb4c38c41aea64310170fd21e1ca4d02bafb39
julgachancipa <julianaga01@gmail.com>
2019-08-05 16:27:01 +0000

    #Feature: frees and sets the head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint2 - The function sets the head to NULL.
+ * @head: head pointer.
+ * Return: ntg.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+	listint_t *current = *head;
+
+	while (current != NULL)
+	{
+		aux = current->next;
+		free(current);
+		current = aux;
+	}
+	*head = NULL;
+}
