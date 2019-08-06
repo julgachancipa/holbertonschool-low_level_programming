b99359d5f1dab33bfdbcea899b5a3ac00653abc0
julgachancipa <julianaga01@gmail.com>
2019-08-06 18:22:07 +0000

    #Fix: fixing the return

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			{
 				new->next = *head;
 				*head = new;
-				return (*head);
 			}
 			else if (current->next == NULL)
 				new->next = NULL;
