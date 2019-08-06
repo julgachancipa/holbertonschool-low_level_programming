db104c3f9c0e8a4a81ecaf2b2f7bcc12fb90389e
julgachancipa <julianaga01@gmail.com>
2019-08-06 23:41:58 +0000

    body

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes the idx node.
+ * @head: pointer to the head pointer.
+ * @index: idx node.
+ * Return: 1 or -1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *current;
+
+	if (head == NULL)
+		return (-1);
+}
