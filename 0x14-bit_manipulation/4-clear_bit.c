c188d1ee082c85a36aa77ba4de382bc037137d1e
julgachancipa <julianaga01@gmail.com>
2019-08-09 01:03:55 +0000

    #Feature: sets the value of a bit to 0 at a given index.

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * pof - pows a number
+ * @x: number
+ * @n: power
+ * Return: result
+ */
+unsigned int pof(unsigned int x, unsigned int n)
+{
+	unsigned int i, result = 1;
+
+	if (n == 0)
+		return (1);
+	for (i = 1; i <= n; i++)
+		result *= x;
+	return (result);
+}
+/**
+ * clear_bit -  sets the value of a bit to 1 at a given index.
+ * @n: number
+ * @index: position
+ * Return: 1 or -1 if it fails.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 64)
+		return (-1);
+	if (*n)
+		*n = *n - pof(2, index);
+	return (1);
+}
