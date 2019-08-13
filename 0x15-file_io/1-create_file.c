4b8594e38464d007de269bdc6534b9a53ce1d8ed
julgachancipa <julianaga01@gmail.com>
2019-08-13 21:53:05 +0000

    #Fix: output 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,11 +10,8 @@ size_t _strlen(char *s)
 {
 	size_t len = 0;
 
-	if (*s != '\0')
-	{
-		len++;
-		return (len + _strlen(s + 1));
-	}
+	for (len = 0; s[len]; len++)
+		;
 	return (len);
 }
 /**
@@ -27,7 +24,6 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t error_;
 	int fd;
-	size_t sz = _strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
@@ -35,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
-		error_ = write(fd, text_content, sz);
+		error_ = write(fd, text_content, _strlen(text_content));
 	close(fd);
 	if (error_ == -1)
 		return (-1);
