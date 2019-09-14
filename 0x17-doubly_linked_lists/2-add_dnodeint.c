1addacf96383f0a5b14fa30b3ec03d4874cbd23b
julgachancipa <julianaga01@gmail.com>
2019-09-14 12:09:46 -0500

    v[1] - (Fix) head node = NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	new->n = n;
 	new->prev = NULL;
+	if (!head)
+	{
+		new->next = NULL;
+		*head = new;
+		return (*head);
+	}
 	new->next = *head;
 	*head = new;
 	return (new);
