2c2aae74200d8e807707df5b72eb8940a4a777f4
julgachancipa <julianaga01@gmail.com>
2019-09-15 13:52:12 -0500

    v[0] - add node at the idx pos. in a double linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,7 +8,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *current, *new;
+	unsigned int i = 0;
+
 	if (idx == 0)
-		return(add_dnodeint(&head, n));
-	els
+		return (add_dnodeint(h, n));
+	current = *h;
+	while (current != NULL)
+	{
+		if (i == idx - 1)
+			break;
+		current = current->next;
+		i++;
+	}
+	if (current == NULL)
+		return (NULL);
+	if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->prev = current;
+	new->next = current->next;
+	(current->next)->prev = new;
+	current->next = new;
+	return (new);
 }
