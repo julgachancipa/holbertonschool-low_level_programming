b851ef27464ce67f0b3d89860064779e251a453f
julgachancipa <julianaga01@gmail.com>
2019-10-11 16:27:36 -0500

    [FIX] remove condition

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
-	if (!table)
+	if (table == NULL)
 		return (NULL);
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t) * size);
-	if (!table->array)
-	{
-		free(table);
-		return (NULL);
-	}
 	return (table);
 }
