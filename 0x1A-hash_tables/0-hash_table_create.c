63722b4e79f45254bae1ccf2e7d897d6ee2491a1
julgachancipa <julianaga01@gmail.com>
2019-10-11 21:46:31 -0500

    [FIX] init. NULL

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,11 +6,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 	return (table);
 }
