5f3a0d1e9b6181f004dcb91ec6b9e67e7c3fd06b
julgachancipa <julianaga01@gmail.com>
2019-10-26 21:47:01 +0000

    NO FINISHED

--- 0x1B-sorting_algorithms/0-main.c
+++ 0x1B-sorting_algorithms/0-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -9,13 +9,21 @@
  */
 int main(void)
 {
-	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	clock_t start, end;
+	double cpu_time_used;
+
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7, 1, 3};
 	size_t n = sizeof(array) / sizeof(array[0]);
+     
+	start = clock();
 
 	print_array(array, n);
 	printf("\n");
 	bubble_sort(array, n);
 	printf("\n");
 	print_array(array, n);
+	end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("%f", cpu_time_used);
 	return (0);
 }
---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current;
+	int i, j, a_prev, a_next, b_prev, b_next;
+
+	current = list;
+	for (i = 0; current; i++)
+	{
+		for (j = i; j > 0; j--)
+		{
+			
+		}
+	}
+}
