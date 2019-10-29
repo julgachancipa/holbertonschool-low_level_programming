16905d36bb867861a3fa8d3fa440ba3aba94e61e
julgachancipa <julianaga01@gmail.com>
2019-10-29 10:35:25 -0500

    [FEATURE] - quicksort

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -1,4 +1,12 @@
 #include "sort.h"
+/**
+ * pivoting - swap th values and evaluate the new pivot
+ * @array: integer array
+ * @low: lower idx
+ * @high: higher idx
+ * @size: array's size
+ * Return: new pivot
+ */
 size_t pivoting(int *array, int low, int high, size_t size)
 {
 	int pivot, aux, i, j;
@@ -10,10 +18,13 @@ size_t pivoting(int *array, int low, int high, size_t size)
 		if (array[j] < pivot)
 		{
 			i++;
-			aux = array[i];
-			array[i] = array[j];
-			array[j] = aux;
-			print_array(array, size);
+			if (j != i)
+			{
+				aux = array[i];
+				array[i] = array[j];
+				array[j] = aux;
+				print_array(array, size);
+			}
 		}
 	}
 	if (pivot < array[i + 1])
@@ -25,6 +36,14 @@ size_t pivoting(int *array, int low, int high, size_t size)
 	}
 	return (i + 1);
 }
+/**
+ * quick_call - recursive function to divide the array
+ * @array: integer array
+ * @low: lower idx
+ * @high: higher idx
+ * @size: array's size
+ * Return: nothing
+ */
 void quick_call(int *array, int low, int high, size_t size)
 {
 	size_t sep;
@@ -32,7 +51,7 @@ void quick_call(int *array, int low, int high, size_t size)
 	if (low < high)
 	{
 		sep = pivoting(array, low, high, size);
-	        quick_call(array, low, sep - 1, size);
+		quick_call(array, low, sep - 1, size);
 		quick_call(array, sep + 1, high, size);
 	}
 }
