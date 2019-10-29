305feef7388bf87308ec90176583075ccb6c95a9
julgachancipa <julianaga01@gmail.com>
2019-10-29 01:56:52 +0000

    NO FINISHED

---
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -0,0 +1,51 @@
+#include "sort.h"
+size_t pivoting(int *array, int low, int high, size_t size)
+{
+	int pivot, aux, i, j;
+
+	pivot = array[high];
+	i = low - 1;
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			aux = array[i];
+			array[i] = array[j];
+			array[j] = aux;
+			print_array(array, size);
+		}
+	}
+	if (pivot < array[i + 1])
+	{
+		aux = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = aux;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+void quick_call(int *array, int low, int high, size_t size)
+{
+	size_t sep;
+
+	if (low < high)
+	{
+		sep = pivoting(array, low, high, size);
+	        quick_call(array, low, sep - 1, size);
+		quick_call(array, sep + 1, high, size);
+	}
+}
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * @array: integer array
+ * @size: array's size
+ * Return: nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array && size > 1)
+	{
+		quick_call(array, 0, size - 1, size);
+	}
+}
