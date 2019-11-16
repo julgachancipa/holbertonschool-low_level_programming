81f33266e509b93cc85f53eed7f218448371edba
julgachancipa <julianaga01@gmail.com>
2019-11-15 23:56:05 -0500

    [Feature] - island_perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+5-island perimeter
+"""
+
+
+def turn_grid(grid):
+    """
+    turn a grid
+    """
+    new_grid = []
+    for n in range(0, len(grid)):
+        new_row = []
+        for row in grid:
+            new_row.append(row[n])
+        new_grid.append(new_row)
+    return new_grid
+
+
+def side_counter(grid):
+    """
+    count horizontal sides
+    """
+    count = 0
+    for i in range(0, len(grid) - 1):
+        for j in range(0, len(grid[i])):
+            if grid[i][j] != grid[i + 1][j]:
+                count += 1
+    return count
+
+
+def island_perimeter(grid):
+    """
+    return the add of all sides
+    """
+    turned_grid = turn_grid(grid)
+    a = side_counter(grid)
+    b = side_counter(turned_grid)
+    return a + b
