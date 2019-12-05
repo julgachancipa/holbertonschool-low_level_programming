4ff783feb5d4c1919702078ca8d52d07374e77a4
julgachancipa <julianaga01@gmail.com>
2019-12-05 17:58:09 -0500

    [FEATURE] performs a left-rotation on a binary tree

---
+++ 0x1D-binary_trees/103-binary_tree_rotate_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *nw_root, *node, *aux;
+
+	if (!tree)
+		return (NULL);
+	nw_root = tree->right;
+	if (nw_root->left)
+		aux = nw_root->left;
+	else
+		aux = NULL;
+	node = tree;
+
+	nw_root->left = node;
+	node->parent = nw_root;
+
+	nw_root->parent = NULL;
+
+	if (aux)
+	{
+		node->right = aux;
+		aux->parent = node;
+	}
+	else
+	{
+		node->right = NULL;
+		node->left = NULL;
+	}
+
+	tree = nw_root;
+	return(tree);
+}
